ef05d0839769e1437623b265396ed6474390aab2
Benny <robert.benford@gfk.com>
2015-06-18 13:31:32 -0400

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to get and set matrix values then potentially compute these values
+## more quickly and efficiently
 
-## Write a short comment describing this function
+## First function takes matrix as an arguement gets and sets the matrix values
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                        x <<- y
+                        m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(matrix) m <<- matrix
+        getmatrix <- function() m
+        list(set = set, get = get,
+              setmatrix = setmatrix,
+              getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## function computes the values for the matrx from prior function but checks 
+## the cache first
 
 cacheSolve <- function(x, ...) {
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- matrix(data, ...)
+        x$setmatrix(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
